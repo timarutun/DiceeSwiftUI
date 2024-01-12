81040df506e2c30a3b3b803aed06e7b16b09bb80
Timur Arutiunian <arutiunian.timur@yahoo.com>
2024-01-12 01:46:07 -0500

    Initial Commit

---
+++ DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by Timur on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
